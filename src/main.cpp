7c87b62b3ebe2b3a203d25221059fb63362b7506
Fabian Hanke <dev-faha@t-online.de>
2018-06-13 14:51:15 +0200

    publish obstacles

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,22 @@
 #include <ros/ros.h>
+#include <cmath>
+#include <sensor_msgs/Image.h>
 #include <drive_ros_msgs/BoundingBoxArray.h>
 #include <drive_ros_msgs/Homography.h>
+#include <drive_ros_msgs/ObstacleArray.h>
 #include <drive_ros_image_recognition/common_image_operations.h>
-#include <visualization_msgs/MarkerArray.h>
-#include <sensor_msgs/Image.h>
 
 drive_ros_image_recognition::ImageOperator* image_operator;
-ros::Publisher pub_marker;
-ros::Publisher pub_image;
-
+ros::Publisher pub_obs;
 std::string output_frame;
 
 
 void bbCallback(const drive_ros_msgs::BoundingBoxArrayConstPtr& msg)
 {
-    visualization_msgs::MarkerArray array;
-
-
-
-
+    drive_ros_msgs::ObstacleArray obs;
 
     for(auto i=msg->boxes.begin(); i != msg->boxes.end(); i++)
     {
-        // set frame
-        std::string frame = output_frame.empty() ? i->header.frame_id : output_frame;
 
         std::vector<cv::Point2f> imagePoints, worldPoints;
         // bottom left
@@ -33,12 +26,35 @@ void bbCallback(const drive_ros_msgs::BoundingBoxArrayConstPtr& msg)
 
         if(image_operator->imageToWorld(imagePoints, worldPoints))
         {
+            ROS_ASSERT(2 == worldPoints.size());
 
+            drive_ros_msgs::Obstacle ob;
+            ob.header.stamp = i->header.stamp;
+            ob.header.frame_id = output_frame.empty() ? i->header.frame_id : output_frame;
+            ob.obstacle_type = drive_ros_msgs::Obstacle::TYPE_CAMERA;
+            ob.trust = i->confidence;
+            ob.width = cv::norm(worldPoints.at(0) - worldPoints.at(1));
+
+            geometry_msgs::Point centroid;
+            centroid.x = ((worldPoints.at(0) + worldPoints.at(1))/2).x;
+            centroid.y = ((worldPoints.at(0) + worldPoints.at(1))/2).y;
+            centroid.z = 0;
+            ob.centroid = centroid;
+
+            for(auto it = worldPoints.begin(); it != worldPoints.end(); it++)
+            {
+               geometry_msgs::Point32 pt;
+               pt.x = it->x;
+               pt.y = it->y;
+               pt.z = 0;
+               ob.polygon.points.push_back(pt);
+            }
+
+            obs.obstacles.push_back(ob);
         }
     }
 
-    pub_marker.publish(array);
-
+    pub_obs.publish(obs);
 }
 
 
@@ -51,13 +67,13 @@ int main(int argc, char **argv)
 
   // common image operations
   if(!image_operator->init()) {
-    ROS_WARN_STREAM("Failed to init image_operator");
+    ROS_ERROR_STREAM("Failed to init image_operator");
     return 1;
   }
 
   pnh.param<std::string>("output_frame", output_frame, "");
 
-  pub_marker = pnh.advertise<visualization_msgs::MarkerArray>("output_obj", 100);
+  pub_obs = pnh.advertise<drive_ros_msgs::ObstacleArray>("obs_topic", 100);
   ros::Subscriber sub_bb = pnh.subscribe("bb_topic", 1, bbCallback);
 
   while(ros::ok()){
