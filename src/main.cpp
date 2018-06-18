e7e6dcf62fd4f2c0c262e46b82eccc4df453b8fb
Fabian Hanke <dev-faha@t-online.de>
2018-06-19 00:45:50 +0200

    adapt to new obstacle msg

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,19 @@
 #include <drive_ros_msgs/ObstacleArray.h>
 #include <drive_ros_image_recognition/common_image_operations.h>
 
+// image operator
 drive_ros_image_recognition::ImageOperator* image_operator;
+
+// publisher
 ros::Publisher pub_obs;
+
+// output frame (if empty we use from message)
 std::string output_frame;
+
+// covariances
+boost::array<float, 36> pose_covariance;
+boost::array<float, 36> twist_covariance;
+
 
 void bbCallback(const drive_ros_msgs::BoundingBoxArrayConstPtr& msg)
 {
@@ -38,13 +48,19 @@ void bbCallback(const drive_ros_msgs::BoundingBoxArrayConstPtr& msg)
             ob.width = cv::norm(worldPoints.at(0) - worldPoints.at(1));
             ob.height = 0;
             ob.length = 0;
-            ob.yaw = 0;
 
-            geometry_msgs::Point32 centroid;
+            geometry_msgs::Quaternion q;
+            q.x=0; q.y=0; q.z=0; q.w=0;
+            ob.centroid_pose.pose.orientation = q;
+
+            geometry_msgs::Point centroid;
             centroid.x = ((worldPoints.at(0) + worldPoints.at(1))/2).x;
             centroid.y = ((worldPoints.at(0) + worldPoints.at(1))/2).y;
             centroid.z = 0;
-            ob.centroid = centroid;
+            ob.centroid_pose.pose.position = centroid;
+
+            ob.centroid_pose.covariance = pose_covariance;
+            ob.centroid_twist.covariance = twist_covariance;
 
             for(auto it = worldPoints.begin(); it != worldPoints.end(); it++)
             {
@@ -76,7 +92,27 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  pnh.param<std::string>("output_frame", output_frame, "");
+  bool ret = true;
+
+  // load covariances
+  std::vector<float> pose_cov_vec, twist_cov_vec;
+  ret &= pnh.getParam("pose_covariance",   pose_cov_vec);
+  ret &= pnh.getParam("twist_covariance",  twist_cov_vec);
+
+  ROS_ASSERT(36 == pose_cov_vec.size());
+  ROS_ASSERT(36 == twist_cov_vec.size());
+
+  std::copy(pose_cov_vec.begin(), pose_cov_vec.begin() + 36, pose_covariance.begin());
+  std::copy(twist_cov_vec.begin(), twist_cov_vec.begin() + 36, twist_covariance.begin());
+
+  // load output frame
+  ret &= pnh.param<std::string>("output_frame", output_frame, "");
+
+  if(!ret)
+  {
+      ROS_ERROR("Failed to load parameters!");
+      return 1;
+  }
 
   pub_obs = pnh.advertise<drive_ros_msgs::ObstacleArray>("obs_topic", 100);
   ros::Subscriber sub_bb = pnh.subscribe("bb_topic", 1, bbCallback);
